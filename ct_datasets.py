be17ec093452dd474dd2de5a83e5dea80f1666c5
Joseph Paul Cohen <joseph@josephpcohen.com>
2023-07-23 16:37:17 -0700

    add caching

--- ct_datasets.py
+++ ct_datasets.py
@@ -1,21 +1,53 @@
 import pandas as pd
 import nibabel as nib
+import torchxrayvision as xrv
 from tqdm.autonotebook import tqdm
+import numpy as np
+import os
 
+
+def get_data(datasets_str, transform=None, path="/home/users/joecohen/scratch/data-scratch/Totalsegmentator_dataset/", merge=True):
+    
+    datasets = []
+    
+    if "total" in datasets_str:
+        datasets.append(TotalSegmenter_Dataset(path, transform))
+    
+    if merge:
+        dmerge = xrv.datasets.Merge_Dataset(datasets)
+        return dmerge
+    else:
+        return datasets
+
+    
 class TotalSegmenter_Dataset():
     def __init__(self, path, transform=None):
         super().__init__()
         
         self.path = path
+        self.cache_dir = self.path + '/cache/'
         self.transform = transform
-
         self.raw_csv = pd.read_csv(self.path + 'meta.csv', delimiter=';')
         
+        metadata_path = self.path + 'meta_frame.csv.gz'
+        if not os.path.exists(metadata_path):
+            print('creating frame metadata at ' + metadata_path)
+            self.create_meta(metadata_path)
+        self.csv = pd.read_csv(metadata_path) 
         
+        if not os.path.exists(self.cache_dir):
+            print('creating cache at ' + self.cache_dir)
+            self.create_cache()
+        
+        
+        self.pathologies = []
+        self.labels = np.zeros([len(self.csv),0])
+
+            
+    def create_meta(self, filename):
         sizes = []
-        #for image_id in tqdm(self.raw_csv.image_id):
         for i, row in tqdm(self.raw_csv.iterrows(), total=self.raw_csv.shape[0]):
-            g = nib.load(path + row.image_id + "/ct.nii.gz")
+            g = nib.load(self.path + row.image_id + "/ct.nii.gz")
             meta = {
                 'frame': list(range(g.shape[2])),
                 'frame_height':g.shape[0],
@@ -25,10 +57,26 @@ class TotalSegmenter_Dataset():
             meta.update(dict(g.header))
             sizes.append(meta)
             
-        self.csv = pd.DataFrame(sizes)
-        self.csv = self.csv.explode('frame')
+        csv = pd.DataFrame(sizes).explode('frame')
+        csv.to_csv(filename, index=None)
         
 
+    def create_cache(self):
+        os.mkdir(self.cache_dir)
+        
+        for image_id, row in tqdm(self.csv.groupby('image_id')):
+            path = self.path + image_id + "/ct.nii.gz"
+            g = nib.load(path)
+            volume = g.get_fdata()
+            
+            for frame_idx in row.frame:
+                cache_path = self.cache_dir + f'/{image_id}_{frame_idx}.npz'
+                np.savez_compressed(cache_path, volume[:,:,frame_idx])
+                
+                
+        
+    
+    
     def string(self):
         return self.__class__.__name__ + " num_samples={}".format(len(self))
 
@@ -37,7 +85,12 @@ class TotalSegmenter_Dataset():
 
     def __getitem__(self, idx):
         
+        item = {}
         sample = self.csv.iloc[idx]
-        g = nib.load(self.path + sample['image_id'] + "/ct.nii.gz")
-        volume = g.get_fdata()
-        return volume[:,:,sample['frame']]
+        
+        cache_path = self.cache_dir + f'/{sample["image_id"]}_{sample["frame"]}.npz'
+        item['img'] = np.load(cache_path)['arr_0']
+
+        if self.transform:
+            item['img'] = self.transform(item['img'])
+        return item
